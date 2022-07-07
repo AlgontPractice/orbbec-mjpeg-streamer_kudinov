8ed882c675d79afe7cc5147e7286e63dc35c68c3
trimanul <ivanko150702@gmail.com>
2022-07-07 13:47:04 +0300

    реализация потока с картой глубины, расстоянием

--- orbbec_mjpeg_streamer/api/mjpeg_handler_service.py
+++ orbbec_mjpeg_streamer/api/mjpeg_handler_service.py
@@ -2,6 +2,7 @@ import logging
 import time
 import numpy as np
 import asyncio
+import cv2
 from aiohttp import web, MultipartWriter
 from aiohttp_cors import CorsViewMixin
 from m7_aiohttp.util.logged import logged
@@ -14,11 +15,8 @@ class MjpegHandlerService(CorsViewMixin):
 
     @logged(logger)
     async def mjpeg_handler_rgb(self, request):
-
-
-
         # TODO: реализуем метод, генерирующий mjpeg-поток на основе кадров из переменной app['frame']
-        my_boundary = 'some-boundary'
+        my_boundary = 'boundary'
         response = web.StreamResponse(
             status=200,
             reason='OK',
@@ -31,6 +29,46 @@ class MjpegHandlerService(CorsViewMixin):
             frame = np.array(request.app['frame']).tobytes()
             with MultipartWriter('image/jpeg', boundary=my_boundary) as mpwriter:
                 mpwriter.append(frame, {
+                    'Content-Type': 'image/jpeg'
+                })
+                await mpwriter.write(response, close_boundary=False)
+            await response.drain()
+
+    @logged(logger)
+    async def mjpeg_handler_depth(self, request):
+        my_boundary = 'boundary'
+        response = web.StreamResponse(
+            status=200,
+            reason='OK',
+            headers={
+                'Content-Type': 'multipart/x-mixed-replace;boundary={}'.format(my_boundary)
+            }
+        )
+        await response.prepare(request)
+        while True:
+            frame = np.array(request.app['depthmap']).tobytes()
+            with MultipartWriter('image/jpeg', boundary=my_boundary) as mpwriter:
+                mpwriter.append(frame, {
+                    'Content-Type': 'image/jpeg'
+                })
+                await mpwriter.write(response, close_boundary=False)
+            await response.drain()
+
+    @logged(logger)
+    async def mjpeg_handler_min(self, request):
+        my_boundary = 'boundary'
+        response = web.StreamResponse(
+            status=200,
+            reason='OK',
+            headers={
+                'Content-Type': 'multipart/x-mixed-replace;boundary={}'.format(my_boundary)
+            }
+        )
+        await response.prepare(request)
+        while True:
+            img = np.array(request.app['minimal']).tobytes()
+            with MultipartWriter('image/jpeg', boundary=my_boundary) as mpwriter:
+                mpwriter.append(img, {
                     'Content-Type': 'image/jpeg'
                 })
                 await mpwriter.write(response, close_boundary=False)
--- orbbec_mjpeg_streamer/scanner/scanner.py
+++ orbbec_mjpeg_streamer/scanner/scanner.py
@@ -2,6 +2,8 @@ import logging
 import cv2
 import asyncio
 
+import numpy as np
+
 
 logger = logging.getLogger('orbbec-mjpeg-streamer')
 
@@ -10,19 +12,38 @@ class Scanner:
 
     def __init__(self, video_params: dict):
         self._video_params = video_params
-        self.cap = None
+        self.colrCap = None
+        self.depthCap = None
 
     async def init_device(self):
         # TODO: метод, в котором реализуем подключение к камере с помощью библиотеки opencv-python
-        self.cap = cv2.VideoCapture(0)
-        if not self.cap.isOpened():
+        self.depthCap = cv2.VideoCapture(cv2.CAP_OPENNI2_ASTRA)
+        self.colorCap = cv2.VideoCapture(1)
+
+
+        if not (self.depthCap.isOpened()):
             raise Exception()
 
     async def image_grabber(self, app):
         while True:
         # TODO: метод, в котором мы получаем кадры с камеры и сохраняем их в переменную app['frame'] в формате jpg
-            status, frame = self.cap.read()
-            if not status:
+            self.colorCap.grab()
+            self.depthCap.grab()
+
+            depthStatus, depthMap = self.depthCap.retrieve(cv2.CAP_OPENNI_DEPTH_MAP)
+            clrStatus, frame = self.colorCap.retrieve()
+            minimal = np.array(depthMap).max()
+            min_img = np.zeros((512,512,3), np.uint8)
+            cv2.putText(min_img, str(minimal), (10,500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 2)
+
+            depthMap = depthMap.astype(np.uint8)
+            if not (clrStatus):
                 raise Exception()
+
+
+            
             app['frame'] = cv2.imencode('.jpg', frame)[1]
-            await asyncio.sleep(1/30)
+            app['depthmap'] = cv2.imencode('.jpg', depthMap)[1]
+            app['minimal'] = cv2.imencode('.jpg', min_img)[1]
+
+            await asyncio.sleep(1/self._video_params['fps'])
